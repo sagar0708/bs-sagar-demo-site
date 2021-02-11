5bea802624eed9059ae29d0f3f8fba6dc2fe1586
sagar chandresha <sagarchandresha0708@gmail.com>
2021-02-11 11:59:49 +0530

    add again all file

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.test-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        dots:true,
+        prevArrow: null,
+        nextArrow: null,
+    });
+    $('#submit_btn').on("click",function(event){
+        event.preventDefault();
+        let b_time = $("#book_time").val();
+        let b_date = $("#book_date").val();
+        let num_of_p = $("#num_of_people").val();
+        alert("Your Booking Time : " + b_time + "\n" +
+            "Your Booikng Date : " + b_date + "\n" + 
+            "Number of People : " + num_of_p);
+    })
+});
+function loader(){
+    // PAGE IS FULLY LOADED  
+    // FADE OUT YOUR OVERLAYING DIV
+    $('.loader').fadeOut();
+    $('.loader').removeClass("d-block");
+    $('.loader').addClass("d-none");
+ }
